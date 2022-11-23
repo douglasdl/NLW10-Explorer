e1a2cfc914b0148fd6cb134e20048990b8e58f82
douglasdl <douglas_san@hotmail.com>
2022-11-23 11:48:08 -0300

    Add javascript functions

---
+++ games.js
@@ -0,0 +1,34 @@
+export const gamesList = {
+    date: new Date('20/11/2022'),
+    games: [
+        {
+            player1: 'qatar',
+            player2: 'ecuador',
+            hour: '13:00',
+            goals1: '0',
+            goals2: '2'
+        },
+        {
+            player1: '',
+            player2: '',
+            hour: '',
+            goals1: '',
+            goals2: ''
+        },
+        {
+            player1: '',
+            player2: '',
+            hour: '',
+            goals1: '',
+            goals2: ''
+        },
+        {
+            player1: '',
+            player2: '',
+            hour: '',
+            goals1: '',
+            goals2: ''
+        }
+    ]
+    
+}
---
+++ main.js
@@ -0,0 +1,37 @@
+function createGame(player1, hour, player2) {
+    return `
+        <li>
+            <img src="./assets/flags/${player1}.svg" alt="Bandeira do ${player1}">
+            <strong>${hour}</strong>
+            <img src="./assets/flags/${player2}.svg" alt="Bandeira da ${player2}">
+        </li>
+    `
+}
+
+let delay = -0.3;
+function createCard(date, day, games) {
+    delay = delay + 0.3;
+    return `
+        <div class="card" style="animation-delay: ${delay}s">
+            <h2>${date} <span>${day}</span></h2>
+
+            <ul>
+                ${games}
+            </ul>
+        </div>
+    `
+}
+document.querySelector('#cards').innerHTML =
+    createCard('24/11', 'quinta', 
+        createGame('switzerland', '07:00', 'cameroon') +
+        createGame('uruguay', '10:00', 'korea') +
+        createGame('portugal', '13:00', 'ghana') +
+        createGame('brazil', '16:00', 'serbia')
+    ) +
+    createCard('28/11', 'segunda',
+        createGame('brazil', '13:00', 'switzerland')
+    ) +
+    createCard('02/12', 'sexta',
+        createGame('cameroon', '16:00', 'brazil')
+    )
+;
