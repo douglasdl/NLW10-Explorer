02ed1ee3e3652bfb6e9b8f0354c7c46c0dfc377d
douglasdl <douglas_san@hotmail.com>
2022-12-04 01:21:06 +0900

    Update list and map function

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import { gamesList } from './games';
-
-function createGame(player1, hour, player2) {
+function createGame(player1, goals1, hour, goals2, player2) {
     return `
         <li>
             <img src="./assets/flags/${player1}.svg" alt="Bandeira do ${player1}">
+            <span>${goals1}</span>
             <strong>${hour}</strong>
+            <span>${goals2}</span>
             <img src="./assets/flags/${player2}.svg" alt="Bandeira da ${player2}">
         </li>
     `
@@ -23,6 +23,44 @@ function createCard(date, day, games) {
         </div>
     `
 }
+
+
+function formatedDate(stringDate) {
+    let UTCdate = new Date(stringDate);
+    let date = ("0" + UTCdate.getDate()).slice(-2);
+    let month = ("0" + (UTCdate.getMonth() + 1)).slice(-2);
+    return date + '/' + month;
+}
+
+function getWeekDay(stringDate) {
+    let UTCdate = new Date(stringDate);
+    let day = UTCdate.getDay();
+    let weekDays = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'];
+    return weekDays[day];
+}
+
+function getTime(stringDate) {
+    let UTCdate = new Date(stringDate);
+    let HH = ("0" + UTCdate.getHours()).slice(-2);
+    let MM = ("0" + UTCdate.getMinutes()).slice(-2); 
+    return HH + ':' + MM;
+}
+
+document.querySelector('#cards').innerHTML =
+
+    gamesList.map((game) => {
+        return (
+            createCard(formatedDate(game.date), getWeekDay(game.date), 
+                createGame(game.teams[0], game.score[0], getTime(game.date), game.score[1], game.teams[1]))
+            
+        )
+    })
+    
+;
+
+
+
+/*
 document.querySelector('#cards').innerHTML =
     createCard('24/11', 'quinta', 
         createGame('switzerland', '07:00', 'cameroon') +
@@ -37,3 +75,4 @@ document.querySelector('#cards').innerHTML =
         createGame('cameroon', '16:00', 'brazil')
     )
 ;
+*/
